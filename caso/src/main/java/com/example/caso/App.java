232eb8e57ea2e5f05718a8256aed33bdd9baafc2
JOSE IGNACIO VARGAS JARAMILLO <135558719+Getz7@users.noreply.github.com>
2023-07-16 13:54:29 -0600

    setup de escena

--- caso/src/main/java/com/example/caso/App.java
+++ caso/src/main/java/com/example/caso/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("App-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("RegistrarNave-view"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- caso/src/main/java/com/example/caso/RegistrarNaveController.java
+++ caso/src/main/java/com/example/caso/RegistrarNaveController.java
@@ -1,4 +1,5 @@
 package com.example.caso;
 
 public class RegistrarNaveController {
+
 }

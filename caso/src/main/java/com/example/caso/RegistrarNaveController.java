cbae059da8192481d70f418ee248a5e28c392af2
JOSE IGNACIO VARGAS JARAMILLO <135558719+Getz7@users.noreply.github.com>
2023-07-20 16:24:04 -0600

    actualizacion trabajo JI

--- caso/src/main/java/com/example/caso/RegistrarNaveController.java
+++ caso/src/main/java/com/example/caso/RegistrarNaveController.java
@@ -16,6 +16,24 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+
+
+import casoestudio.Gestores.Gestor;
+import casoestudio.Gestores.GestorBuilder;
+import casoestudio.constructores.ConstructorConcreto;
+import casoestudio.objetos.*;
+import casoestudio.producto_Concreto._UsuarioDAO;
+import casoestudio.producto_Concreto._Usuarios;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 

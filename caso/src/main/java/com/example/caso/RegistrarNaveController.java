c950dc55155dd0ca2c0fe90570c61b5c62793ade
JOSE IGNACIO VARGAS JARAMILLO <135558719+Getz7@users.noreply.github.com>
2023-07-19 20:06:40 -0600

    combo box

--- caso/src/main/java/com/example/caso/RegistrarNaveController.java
+++ caso/src/main/java/com/example/caso/RegistrarNaveController.java
@@ -3,22 +3,26 @@ package com.example.caso;
 import casoestudio.Gestores.Gestor;
 import casoestudio.constructores.ConstructorConcreto;
 import casoestudio.objetos.*;
+import casoestudio.producto_Concreto._UsuarioDAO;
+import casoestudio.producto_Concreto._Usuarios;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RegistrarNaveController {
-
+    @FXML
+    private ComboBox<String> nombreCombo;
+    @FXML
+    private ComboBox<String> catCombo;
+    private _UsuarioDAO usuarioDAO;
+    private CategoriaDAO catDAO;
     @FXML
     private TableView <MarcaModelo>  tablaMM;
     @FXML
@@ -35,11 +39,15 @@ public class RegistrarNaveController {
         marcaCol.setCellValueFactory(new PropertyValueFactory<>("marcaId"));
         modeloCol.setCellValueFactory(new PropertyValueFactory<>("modeloId"));
         anioCol.setCellValueFactory(new PropertyValueFactory<>("anio"));
-
-
         MarcaModeloDAO marcaModeloDAO = new MarcaModeloDAO();
         List<MarcaModelo> marcaModeloList = marcaModeloDAO.getMarcaModeloData();
         tablaMM.setItems(FXCollections.observableArrayList(marcaModeloList));
+        usuarioDAO = new _UsuarioDAO();
+        catDAO= new CategoriaDAO();
+        List<String> usuarios = usuarioDAO.getUsuarios();
+        nombreCombo.getItems().addAll(usuarios);
+        List<String> categorias = catDAO.getCategorias();
+        catCombo.getItems().addAll(categorias);
 
 
 

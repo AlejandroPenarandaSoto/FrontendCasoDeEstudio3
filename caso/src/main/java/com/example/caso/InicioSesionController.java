c05cec5921b0b1af8fed952bfc3ebae7e9a602da
danielito0428 <dgnavarro06@gmail.com>
2023-07-21 21:49:46 -0600

    insert del usario y contrasenna del usario y logica del login a medias

--- caso/src/main/java/com/example/caso/InicioSesionController.java
+++ caso/src/main/java/com/example/caso/InicioSesionController.java
@@ -1,4 +1,52 @@
 package com.example.caso;
 
+import casoestudio.Gestores.Gestor;
+import casoestudio.producto_Concreto._UsuarioDAO;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class InicioSesionController {
+
+    @FXML
+    private TextField txtUser,txtPswd;
+
+    @FXML
+    private Label txtError;
+
+
+    private Gestor appGestor = new Gestor();
+
+    @FXML
+    protected void onBtnInicioSesion(ActionEvent event) throws IOException {
+
+
+        if (txtUser.getText().equals(appGestor.getUsername(txtUser.getText()))){
+
+            if (appGestor.getUsuarioIdByUser(txtUser.getText()) == 1){
+                Parent newPageParent = FXMLLoader.load(getClass().getResource("MenuCliente-view.fxml"));
+                Scene newPageScene = new Scene(newPageParent);
+                Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                currentStage.setScene(newPageScene);
+                currentStage.show();
+            } else if (appGestor.getUsuarioIdByUser(txtUser.getText()) == 2) {
+                Parent newPageParent = FXMLLoader.load(getClass().getResource("MenuVendedor-view.fxml"));
+                Scene newPageScene = new Scene(newPageParent);
+                Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                currentStage.setScene(newPageScene);
+                currentStage.show();
+            }
+
+
+        }else txtError.setText("Usuario o Contrasenna incorrecta!");
+
+    }
 }
--- caso/src/main/java/com/example/caso/MenuClienteController.java
+++ caso/src/main/java/com/example/caso/MenuClienteController.java
@@ -1,4 +1,67 @@
 package com.example.caso;
 
-public class MenuClienteController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuClienteController implements Initializable {
+
+    @FXML
+    private Button btnRegistrarNave,btnInvCliente,btnInvVendedor,btnCerrarSesion;
+
+    @FXML
+    private Text txtCliente;
+
+    @FXML
+    protected void onPressbtnRegistroNave(ActionEvent event) throws IOException{
+        Parent newPageParent = FXMLLoader.load(getClass().getResource("RegistroNave-view.fxml"));
+        Scene newPageScene = new Scene(newPageParent);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(newPageScene);
+        currentStage.show();
+    }
+
+    @FXML
+    protected void onPressbtnInvCliente(ActionEvent event) throws IOException{
+        Parent newPageParent = FXMLLoader.load(getClass().getResource("InventarioCliente-view.fxml"));
+        Scene newPageScene = new Scene(newPageParent);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(newPageScene);
+        currentStage.show();
+    }
+
+    @FXML
+    protected void onPressbtnInvVendedor(ActionEvent event) throws IOException{
+        Parent newPageParent = FXMLLoader.load(getClass().getResource("InventarioVendedor-view.fxml"));
+        Scene newPageScene = new Scene(newPageParent);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(newPageScene);
+        currentStage.show();
+    }
+
+    @FXML
+    protected void onPressbtnCerrarSesion(ActionEvent event) throws IOException{
+        Parent newPageParent = FXMLLoader.load(getClass().getResource("InicioSesion-view.fxml"));
+        Scene newPageScene = new Scene(newPageParent);
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(newPageScene);
+        currentStage.show();
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
